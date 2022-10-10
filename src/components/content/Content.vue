cae5076d75e4d9ebcf954142d3da2e03b84fa4fe
Егор <eremin.e@m-style.spb.ru>
2022-10-10 16:27:01 +0300

    flex

--- src/components/content/Content.vue
+++ src/components/content/Content.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="cont">
-    <div class="content_box"></div>
+    <div class="content_box">
+      <p class="pic">
+        <img src="../../../public/pic.jpg" width="320" height="500" alt="Фотография">
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "Cont"
 
 }
 </script>
+
+<style scoped>
+.pic  {
+  text-align: center;
+}
+</style>
--- src/components/header/Head.vue
+++ src/components/header/Head.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="head">
     <div class="head_box">
+      <div class="big">WANTED</div>
+      <div class="smal">hired or dead</div>
+      
+    
     </div>
   </div>
 </template>
@@ -10,3 +14,25 @@ export default {
   name: "Head"
 }
 </script>
+
+<style scoped>
+@import url('http://fonts.cdnfonts.com/css/grand-duke');
+/* @import '../../styles/head.css'; */
+.head_box {
+  font-family: 'GRAND DUKE', sans-serif;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.smal {
+    font-size: 40px;
+    /* color: brown; */
+}
+.big {
+    font-size: 80px;
+    /* color: brown; */
+}
+</style>
